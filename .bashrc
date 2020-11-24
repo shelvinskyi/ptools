0da79040a27514874896ab7128ce6eddb8547ec1
acidburnburn <shelvinskyi@pm.me>
2020-11-24 11:16:57 +0000

    deleted unnecessary stuff

--- .bashrc
+++
@@ -1,6 +0,0 @@
-# add colour
-alias ll="ls -Glh"
-alias ls="ls -G"
-alias grep="grep --color=auto"
-
-source $HOME/.powerline.sh
--- install.sh
+++
@@ -1,7 +0,0 @@
-cp -r .utils ~/
-cp -r .vim ~/
-cp .vimrc ~/
-cp .tmux.conf ~/
-cp -r .config/ ~/
-cp .Xmodmap ~/
-cp .Xresources ~/
