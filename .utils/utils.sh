f3830554773ff1252fbc456e5904126d51f504a9
shelvinskyi <shelvinskyi@pm.me>
2021-10-06 10:27:59 +0100

    migrated to bspwm

--- .utils/utils.sh
+++
@@ -1,9 +0,0 @@
-alias ll="ls -Glh --color=auto"
-alias ls="ls -G --color=auto"
-alias grep="grep --color=auto"
-alias df="df -h"
-
-alias weather="curl wttr.in/London"
-alias mobile="echo -n '07548682559' | xclip -selection clipboard"
-alias cal="cal -3"
-alias notes="vim ~/notes"
